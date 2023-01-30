4cd6b055f1324fa898f088895dc5349f94973d62
devbipu <devbipu@gmail.com>
2023-01-30 11:09:52 +0600

    Migration and seeder added for ORM

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\UserAddress;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Make relation with address table by user address id
+     */
+    public function address(){
+        return $this->belongsTo(UserAddress::class, 'address_id');
+    }
 }
